f34638da60f2cd06c3089ee15d2afcc54a986b9d
Mauricio <mauricio@example.com>
2025-11-03 13:01:42 -0300

    Fix: Importar index.css en index.tsx para cargar estilos de Tailwind CSS en producciÃ³n

--- index.tsx
+++ index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
+import './index.css'; // Importar estilos de Tailwind CSS
 import App from './App';
 import { ToastProvider } from './contexts/ToastContext';
 
